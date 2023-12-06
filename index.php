9c13d8fa001393246fcfcc95563138ff10fe6381
JohnMitysumy <johnmitysumy@gmail.com>
2023-12-06 11:50:33 -0300

    Adicionado includes na página incial

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 
+<?php
+
+    function incluirPaginas(){
+        // Caminho para a pasta
+        $pasta = 'Components/';
+
+        // Obter uma lista de arquivos PHP na pasta
+        $arquivos = glob($pasta . '*.php');
+
+        // Loop para incluir cada arquivo
+        foreach ($arquivos as $arquivo) {
+            include $arquivo;
+        }
+    }
+
+    incluirPaginas()
+
+?>
